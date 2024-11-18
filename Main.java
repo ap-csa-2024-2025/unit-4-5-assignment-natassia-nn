b7ff81649c411d662792b12a38d1c1c9232fe913
natassia-nn <natassianguyen@gmail.com>
2024-11-18 10:17:07 -0800

    made changes

--- Main.java
+++ Main.java
@@ -9,11 +9,32 @@ public class Main
 
   public static void printNTimes(String word, int N)
   {
-    // code solution here
+    for (int i = 0; i < word.length(); i++)
+    {
+      String L = word.substring(i, i+1);
+      for (int j = 0; j < N; j++)
+      {
+        System.out.println(L);
+      }
+    }
   }
 
   public static void printNums()
   {
+    for ()
+    {
+      for (int repeater = 0; repeater < 10; repeater++)
+      {
+        System.out.print(10 + " ");
+      }
+      System.out.println();
+    }
+
+    for (int i = 0; i < 9; i++)
+    {
+      System.out.println(9 + " ");
+    }
+    System.out.println();
     // code solution here
   }
 
